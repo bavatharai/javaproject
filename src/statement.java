ad16514d51ed842e0d89a40820614e44948a4562
acer <acer@Z14-55N>
2026-09-02 17:01:12 +0530

    space

--- src/statement.java
+++ src/statement.java
@@ -4,6 +4,8 @@ public class statement {
 	public static void main(String[] args) {
 		int age=16;
 		if(age>=18) {
+			
+			
 		System.out.println("eligible to vote");
 		}else {
 		System.out.println("not eligible to vote");
