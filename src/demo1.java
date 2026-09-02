5490da924479ce61d4daf7ae7025ce014827e14e
acer <acer@Z14-55N>
2026-09-02 17:15:35 +0530

    demo1

---
+++ src/demo1.java
@@ -0,0 +1,13 @@
+
+public class demo1 {
+	public static void main(String[]args) {
+		int age=16;
+		if(age>=18) {
+			System.out.println("eligible to vote");
+		}else {
+				System.out.println("not eligible to vote");
+			}
+		}
+	}
+
+
