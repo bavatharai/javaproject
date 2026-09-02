e081c207a490487df08a2a28a5fbb31833e666b0
acer <acer@Z14-55N>
2026-09-02 17:36:59 +0530

    employee1

---
+++ src/employee1.java
@@ -0,0 +1,11 @@
+
+public class employee1 {
+	public static void main(String[]args) {
+		String empname="bavatharani";
+		System.out.println(empname);
+		int empage=21;
+		System.out.println(empage);
+		
+	}
+
+}
