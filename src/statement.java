85cb100c64094ae21163ddc8e4b8c08ccc454391
acer <acer@Z14-55N>
2026-09-02 17:40:56 +0530

    statement

---
+++ src/statement.java
@@ -0,0 +1,12 @@
+
+public class statement {
+	public  static void main(String[]args) {
+		int age=17;
+		if(age>=18) {
+			System.out.println("eligible to vote");
+		}else {
+			System.out.println("not eligible to vote");
+		}
+	}
+}
+
